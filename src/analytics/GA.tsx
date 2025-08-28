38db2273201d89c1d876d698f6d7887aa300d19f
TheForceofthedragon <bryansmelin@gmail.com>
2025-08-27 22:45:52 -0500

    fix: GA types for Vercel build

--- src/analytics/GA.tsx
+++ src/analytics/GA.tsx
@@ -5,25 +5,36 @@ import { useEffect } from 'react'
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
+declare global {
+  interface Window {
+    dataLayer: unknown[]
+    gtag?: (...args: unknown[]) => void
+  }
+}
+
 export default function GA() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
     if (!GA_ID) return
-    const url = pathname + (searchParams?.toString() ? `?${searchParams}` : '')
-    // @ts-ignore
+    const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : '')
     window.gtag?.('event', 'page_view', { page_path: url })
   }, [pathname, searchParams])
 
   if (!GA_ID) return null
+
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
       <Script id="ga-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){ window.dataLayer.push(arguments); }
+          window.gtag = gtag;
           gtag('js', new Date());
           gtag('config', '${GA_ID}', { send_page_view: false });
         `}
